5105abdc719e760f51a1e9d8ca83d25505ebdcef
JinkownHong <161419351+JinkownHong@users.noreply.github.com>
2024-06-12 20:45:59 +0900

    feat: Global Exception 생성 (#10)

---
+++ src/main/kotlin/com/example/backoffice/common/exception/ModelNotFoundException.kt
@@ -0,0 +1,5 @@
+package com.example.backoffice.common.exception
+
+data class ModelNotFoundException(val modelName: String, val id: Long) : RuntimeException(
+    "Model $modelName not found with given id: $id"
+)
---
+++ src/main/kotlin/com/example/backoffice/common/exception/dto/ErrorResponse.kt
@@ -0,0 +1,5 @@
+package com.example.backoffice.common.exception.dto
+
+data class ErrorResponse(
+    val message: String?,
+)
