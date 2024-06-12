2e0fde59f0f2a30c7e266fdc2fa6adcbcf048e9e
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-06-12 17:26:47 +0900

    feat: controller 작성

---
+++ src/main/kotlin/com/example/backoffice/domain/product/controller/ProductController.kt
@@ -0,0 +1,44 @@
+package com.example.backoffice.domain.product.controller
+
+import com.example.backoffice.domain.product.dto.CreateProductRequestDto
+import com.example.backoffice.domain.product.dto.ProductDetailResponseDto
+import com.example.backoffice.domain.product.dto.ProductResponseDto
+import com.example.backoffice.domain.product.dto.UpdateProductRequestDto
+import com.example.backoffice.domain.product.service.ProductService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/products")
+class ProductController(
+    private val productService: ProductService
+) {
+
+    @GetMapping
+    fun getProducts(): ResponseEntity<List<ProductResponseDto>> {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getProducts())
+    }
+
+    @GetMapping("/{productId}")
+    fun getProduct(@PathVariable("productId") productId: Long):ResponseEntity<ProductDetailResponseDto>{
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getProductById(productId))
+    }
+
+    @PostMapping
+    fun createProduct(@RequestBody request: CreateProductRequestDto) : ResponseEntity<ProductResponseDto>{
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(request))
+    }
+
+    @PutMapping("/{productId}")
+    fun updateProduct(
+        @PathVariable("productId") productId: Long,
+        @RequestBody request: UpdateProductRequestDto) :ResponseEntity<ProductResponseDto>{
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.updateProduct(productId, request))
+    }
+
+    @DeleteMapping("/{productId}")
+    fun deletePost(@PathVariable("productId") productId: Long): ResponseEntity<Unit> {
+        return ResponseEntity.status(HttpStatus.OK).body(productService.deleteProduct(productId))
+    }
+}
