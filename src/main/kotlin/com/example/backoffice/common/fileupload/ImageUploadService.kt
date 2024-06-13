9a1e7894abc638a11f185755da6cbffb5e98778b
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-06-13 16:59:05 +0900

    style: import 정리

---
+++ src/main/kotlin/com/example/backoffice/common/fileupload/ImageUploadService.kt
@@ -0,0 +1,4 @@
+package com.example.backoffice.common.fileupload
+
+class ImageUploadService {
+}
---
+++ src/main/kotlin/com/example/backoffice/common/fileupload/S3/S3Config.kt
@@ -0,0 +1,8 @@
+package com.example.backoffice.common.fileupload.S3
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class S3Config {
+
+}
