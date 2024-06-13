bde154a2f5cc6ca432c9d5103ca525dcc857878e
hongjinkwon <hjk0472@gmail.com>
2024-06-13 14:43:44 +0900

    feat: GlobalExceptionHandler 내 handleInvalidObjectException 메서드 추가

--- src/main/kotlin/com/example/backoffice/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/backoffice/common/exception/GlobalExceptionHandler.kt
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
+import java.io.InvalidObjectException
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
@@ -15,6 +16,13 @@ class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException::class)
     fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(ErrorResponse(message = e.message))
+    }
+
+    @ExceptionHandler(InvalidObjectException::class)
+    fun handleInvalidObjectException(e: InvalidObjectException): ResponseEntity<ErrorResponse> {
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
             .body(ErrorResponse(message = e.message))
