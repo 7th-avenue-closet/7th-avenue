e0204fb4bec4f5a6e96c1b51e511b15384246919
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-06-13 17:54:34 +0900

    feat: s3 설정 및 upload 설정

--- src/main/kotlin/com/example/backoffice/common/fileupload/ImageUploadService.kt
+++ src/main/kotlin/com/example/backoffice/common/fileupload/ImageUploadService.kt
@@ -1,4 +1,33 @@
 package com.example.backoffice.common.fileupload
 
-class ImageUploadService {
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.ObjectMetadata
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import java.io.IOException
+import java.time.ZonedDateTime
+
+@Service
+class ImageUploadService(
+    private val s3Client: AmazonS3Client
+) {
+    @Value("\${BUCKET_NAME}")
+    private lateinit var bucket: String
+
+    fun upload(file: MultipartFile): String? {
+        val image = listOf("jpg", "jpeg", "png", "gif", "bmp")
+        val fileName = file.originalFilename ?: throw IOException("File name is empty")
+        val exception = fileName.substringAfterLast('.')
+        val uploadName = ZonedDateTime.now().toString() + fileName
+        if (!image.contains(exception)) return null
+        val metadata = ObjectMetadata().apply {
+            contentType = "image/${exception}"
+            contentLength = file.size
+        }
+        s3Client.putObject(bucket, uploadName, file.inputStream, metadata)
+        return s3Client.getUrl(bucket, uploadName).toString()
+
+    }
+
 }
