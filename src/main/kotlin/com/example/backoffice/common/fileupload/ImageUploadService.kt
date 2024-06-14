3141bde24e261d62c3c9b76d020cf272a99cb571
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-06-14 15:29:06 +0900

    style: import 정리

--- src/main/kotlin/com/example/backoffice/common/fileupload/ImageUploadService.kt
+++ src/main/kotlin/com/example/backoffice/common/fileupload/ImageUploadService.kt
@@ -3,12 +3,8 @@ package com.example.backoffice.common.fileupload
 import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model.DeleteObjectRequest
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest
-import com.amazonaws.services.s3.model.ObjectMetadata
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import org.springframework.web.multipart.MultipartFile
-import java.io.IOException
-import java.time.LocalDateTime
 import java.util.*
 
 @Service
