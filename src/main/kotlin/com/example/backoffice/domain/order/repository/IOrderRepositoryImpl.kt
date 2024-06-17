3b750f8abf49dc58ede33a3d9ab5f1d9dd8b866f
LSH <fhzktm@naver.com>
2024-06-17 17:14:58 +0900

    refactor : IOrderRepositoryImpl 에서 @Service 를 @Component 로 변경

--- src/main/kotlin/com/example/backoffice/domain/order/repository/IOrderRepositoryImpl.kt
+++ src/main/kotlin/com/example/backoffice/domain/order/repository/IOrderRepositoryImpl.kt
@@ -6,10 +6,11 @@ import com.example.backoffice.domain.product.model.Product
 import com.example.backoffice.domain.product.repository.ProductRepository
 import com.example.backoffice.domain.user.model.User
 import com.example.backoffice.domain.user.repository.UserRepository
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import java.util.*
 
-@Service
+@Component
 class IOrderRepositoryImpl(
     private val orderRepository: OrderRepository,
     private val orderProductRepository: OrderProductRepository
