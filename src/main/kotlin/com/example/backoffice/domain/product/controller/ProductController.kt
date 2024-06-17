fc7366ff87ba4806e2322110f8029142b4273423
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-06-17 09:30:29 +0900

    feat: 상품 조건별 검색 및 페이지네이션 (#44)
    * feat: category, 가격순정렬, 이름순, 최신순
    
    * feat: category, 가격순정렬, 이름순, 최신순
    
    * feat: pagesize, cursor 기본값줌
    
    * feat: 이름으로 검색 추가
    
    * feat: 세일중인거만보기
    
    * fix: 줄정리
    
    * fix: 정렬 값 다른값 들어올 경우 기본정렬이 아니라 예외처리로 변경
    
    * fix: 오래된순으로 정렬할때 커서보다 낮은거부터 가져오기
    
    * fix: cursor 기본값 맥스-커서로 해둠
    
    * fix: cursor 기본값 제거
    
    * feat: cursor sort field에 따라 변경되도록 수정
    
    ---------
    
    Co-authored-by: TONIXX <jiseok95@gmail.com>

--- src/main/kotlin/com/example/backoffice/domain/product/controller/ProductController.kt
+++ src/main/kotlin/com/example/backoffice/domain/product/controller/ProductController.kt
@@ -1,6 +1,7 @@
 package com.example.backoffice.domain.product.controller
 
 import com.example.backoffice.domain.product.dto.*
+import com.example.backoffice.domain.product.model.Category
 import com.example.backoffice.domain.product.service.ProductService
 import com.example.backoffice.infra.security.CustomPreAuthorize
 import com.example.backoffice.infra.security.MemberPrincipal
@@ -18,8 +19,22 @@ class ProductController(
 ) {
 
     @GetMapping
-    fun getProducts(): ResponseEntity<List<ProductResponseDto>> {
-        return ResponseEntity.status(HttpStatus.OK).body(productService.getProducts())
+    fun getProducts(
+        @RequestParam pageSize: Long = 10,
+        @RequestParam sorted: String = "id.desc",
+        @RequestParam cursor: String?,
+        @RequestParam category: Category?,
+        @RequestParam name: String?,
+        @RequestParam onDiscount: Boolean?,
+    ): ResponseEntity<List<ProductResponseDto>> {
+        val cursorValue: Any? = when (sorted.split(".")[0]) {
+            "price", "id" -> cursor?.toLongOrNull()
+            "name" -> cursor
+            else -> throw IllegalArgumentException("Invalid sorted field")
+        }
+        
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(productService.getProducts(pageSize, sorted, cursorValue, category, name, onDiscount))
     }
 
     @GetMapping("/{productId}")
--- src/main/kotlin/com/example/backoffice/domain/product/repository/ProductRepository.kt
+++ src/main/kotlin/com/example/backoffice/domain/product/repository/ProductRepository.kt
@@ -3,8 +3,7 @@ package com.example.backoffice.domain.product.repository
 import com.example.backoffice.domain.product.model.Product
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ProductRepository : JpaRepository<Product, Long> {
-    fun findAllByDeletedAtIsNull(): List<Product>
+interface ProductRepository : JpaRepository<Product, Long>, CustomProductRepository {
     fun findByIdAndDeletedAtIsNull(id: Long): Product?
     fun existsByIdAndDeletedAtIsNull(id: Long): Boolean
 }
