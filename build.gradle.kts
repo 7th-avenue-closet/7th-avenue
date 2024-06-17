6064d1fb59f4a2b7f2564a92c31a7bc801165b21
TONIXX <jiseok95@gmail.com>
2024-06-17 10:11:45 +0900

    test: PreAuthorizeTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,8 @@ repositories {
     mavenCentral()
 }
 
+val kotestVersion = "5.7.2"
+val mockkVersion = "1.13.9"
 val queryDslVersion = "5.0.0"
 
 dependencies {
@@ -32,6 +34,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
     implementation("io.jsonwebtoken:jjwt-api:0.12.5")
     implementation("com.querydsl:querydsl-jpa:$queryDslVersion:jakarta")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta")
 
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
@@ -42,6 +45,12 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
 }
 
 kotlin {
